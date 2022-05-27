406d990f2d707fd402643bdb21ccbf13b31a896b
Gavin Shan <gshan@redhat.com>
2022-05-27 15:35:33 +0800

    linux-headers: Update SDEI capability
    Signed-off-by: Gavin Shan <gshan@redhat.com>

--- linux-headers/linux/kvm.h
+++ linux-headers/linux/kvm.h
@@ -1150,6 +1150,7 @@ struct kvm_ppc_resize_hpt {
 #define KVM_CAP_DISABLE_QUIRKS2 213
 /* #define KVM_CAP_VM_TSC_CONTROL 214 */
 #define KVM_CAP_SYSTEM_EVENT_DATA 215
+#define KVM_CAP_ARM_SDEI 217
 
 #ifdef KVM_CAP_IRQ_ROUTING
 
