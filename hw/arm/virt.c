bb3ac5210a041c56cc8916694ec68aac7c60704e
Jean-Philippe Brucker <jean-philippe@linaro.org>
2024-11-07 17:42:02 +0000

    hw/arm/virt: Add measurement log for confidential boot
    Create a measurement log describing operations performed by QEMU to
    initialize the guest, and load it into guest memory above the DTB.
    
    Cc: Stefan Berger <stefanb@linux.vnet.ibm.com>
    Signed-off-by: Jean-Philippe Brucker <jean-philippe@linaro.org>

--- hw/arm/virt.c
+++ hw/arm/virt.c
@@ -1812,6 +1812,11 @@ void virt_machine_done(Notifier *notifier, void *data)
         exit(1);
     }
 
+    if (vms->event_log) {
+        object_property_set_uint(vms->event_log, "load-addr",
+                                 vms->bootinfo.log_paddr, &error_fatal);
+    }
+
     pci_bus_add_fw_cfg_extra_pci_roots(vms->fw_cfg, vms->bus,
                                        &error_abort);
 
@@ -2145,6 +2150,21 @@ static void virt_cpu_post_init(VirtMachineState *vms, MemoryRegion *sysmem)
                 exit(1);
             }
         }
+    }
+}
+
+static void create_measurement_log(VirtMachineState *vms)
+{
+    Error *err = NULL;
+
+    vms->event_log = kvm_arm_rme_get_measurement_log();
+    if (vms->event_log == NULL) {
+        return;
+    }
+    vms->bootinfo.log_size = object_property_get_uint(vms->event_log,
+                                                      "max-size", &err);
+    if (err != NULL) {
+        error_report_err(err);
     }
 }
 
@@ -2495,6 +2515,8 @@ static void machvirt_init(MachineState *machine)
     }
 
     kvm_arm_rme_init_gpa_space(vms->highest_gpa, vms->bus);
+
+    create_measurement_log(vms);
 
     vms->bootinfo.ram_size = machine->ram_size;
     vms->bootinfo.board_id = -1;
