a79d74a8119788b1fc33bec18bc19217c89a9ab8
Kazuhiro Abe <fj1078ii@aa.jp.fujitsu.com>
2026-06-15 14:19:01 +1000

    target/arm/aarch64-qmp-cmds: Add Arm CCA support
    Add a QMP command to retrieve CCA capabilities;
    supported on aarch64 platforms only.
    
    Signed-off-by: Kazuhiro Abe <fj1078ii@aa.jp.fujitsu.com>
    Signed-off-by: Jean-Philippe Brucker <jean-philippe@linaro.org>
    Signed-off-by: Gavin Shan <gshan@redhat.com>

--- tests/qtest/qmp-cmd-test.c
+++ tests/qtest/qmp-cmd-test.c
@@ -54,6 +54,8 @@ static int query_error_class(const char *cmd)
         { "xen-event-list", ERROR_CLASS_GENERIC_ERROR },
         /* requires firmware with memory buffer logging support */
         { "query-firmware-log", ERROR_CLASS_GENERIC_ERROR },
+        /* Only valid within RME host */
+        { "query-rme-guest-capabilities", ERROR_CLASS_GENERIC_ERROR },
         { NULL, -1 }
     };
     int i;
